2483354e655ae0ceb26b01eeb80be50e4ff5f612
Jeffrey Chang <jeffjar@stanford.edu>
2019-03-18 00:17:51 -0700

    Rephrase research

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
 var pages = ["index.html","research.html", "performances.html", "projects.html",
              "essays.html", "music.html", "gallery.html", "cv.html"]
-var names = ["About", "Research", "Performances", "Projects", "Writings",
+var names = ["Home", "Research", "Performances", "Projects", "Writings",
              "Music", "Media", "CV"];
 var titles = ["About me", "My past and present research", "A list of my performances",
               "My projects", "Some essays", "My past musical compositions",
               "A photo gallery", "Curriculum Vitae"];
 var url_parts = document.location.href.split('/');
 var current_page = url_parts[url_parts.length - 1];
+if (current_page == "") {
+    current_page = "index.html";
+}
 
 var menu_html = "";
 for (var i = 0; i < pages.length; i++) {
