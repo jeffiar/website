e0d28d667de855ae3b437d272b7fadc1809c8793
Jeffrey Chang <jeffjar@stanford.edu>
2019-05-05 12:51:35 -0700

    Random stuff including writing shpeals

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
-var pages = ["index.html","research.html", "performances.html", "projects.html",
-             "essays.html", "music.html", "gallery.html", "cv.html"]
-var names = ["Home", "Research", "Performances", "Projects", "Writings",
+var pages = ["index.html","research.html", "performances.html", "essays.html",
+             "projects.html", "music.html", "gallery.html", "cv.html"]
+var names = ["Home", "Research", "Performances", "Writings", "Projects",
              "Music", "Media", "CV"];
 var titles = ["About me", "My past and present research", "A list of my performances",
-              "My projects", "Some essays", "My past musical compositions",
+              "Some writings", "My projects", "My past musical compositions",
               "A photo gallery", "Curriculum Vitae"];
 var url_parts = document.location.href.split('/');
 var current_page = url_parts[url_parts.length - 1];
@@ -11,14 +11,14 @@ if (current_page == "") {
     current_page = "index.html";
 }
 
-var menu_html = "";
+var parts = []
 for (var i = 0; i < pages.length; i++) {
     if (pages[i] == current_page) {
-        menu_html += '[ ' + names[i] + " ] ";
+        parts.push(names[i]);
     } else {
-        menu_html += '[ <a href="' + pages[i] + '" title="' + titles[i] + '">' + names[i] + '</a> ] ';
+        parts.push('<a href="' + pages[i] + '" title="' + titles[i] + '">' + names[i] + '</a>');
     }
 }
 
-// menu_html += '<br/> [ <a href="statmech2/"> Notes on Statistical Mechanics</a> ]'
+var menu_html = '[ ' + parts.join(' | ') + ' ]'
 document.write(menu_html);
