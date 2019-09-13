f86f9f656297b019df6533c65b006e41d5fa90fa
Jeffrey Chang <jeffjar@stanford.edu>
2019-09-12 23:54:06 -0700

    No JS in menu top bar hehe

--- menu.js
+++ menu.js
@@ -1,10 +1,8 @@
-var pages = ["index.html","research.html", "performances.html", "essays.html",
-             "projects.html", "music.html", "gallery.html", "cv.html"]
-var names = ["Home", "Research", "Performances", "Writings", "Projects",
-             "Music", "Media", "CV"];
+var pages = ["index.html","research.html", "performances.html", "essays.html", "music.html", "cv.html"]
+var names = ["Home", "Research", "Performances", "Writings",
+             "Music", "CV"];
 var titles = ["About me", "My past and present research", "A list of my performances",
-              "Some writings", "My projects", "My past musical compositions",
-              "A photo gallery", "Curriculum Vitae"];
+              "Some writings", "My past musical compositions", "Curriculum Vitae"];
 var url_parts = document.location.href.split('/');
 var current_page = url_parts[url_parts.length - 1];
 if (current_page == "") {
