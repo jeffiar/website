e241b483117d1dd57f7f906db20e7d98a56383e0
Jeffrey Chang <jeffjar@stanford.edu>
2019-09-29 15:25:20 -0700

    door tweaks

--- door.js
+++ door.js
@@ -1,7 +1,6 @@
-var N_DOORS = 5;
 var doors = [];
-var audios = [];
 var doorDiv = document.getElementById("door-div");
+var scoreDiv = document.getElementById("score");
 var openDoorFile = "media/open.png";
 var closedDoorFile = "media/closed.png";
 var gertrudeFile = "media/gertrude.mp3";
@@ -10,6 +9,7 @@ var dt = 100; // in milliseconds
 var n = 0; // number of game ticks so far
 var freq = 0.05;
 var playing = false;
+var score = 0;
 
 function togglePlay() {
     if (playing == false) {
@@ -24,27 +24,23 @@ function togglePlay() {
 function beginPlay() {
     document.getElementById("togglePlay").innerHTML = "Stop this madness!!";
 
-    for(var i = 0; i < N_DOORS; i++) {
-        var door = makeNewDoor();
-        var audio = new Audio(gertrudeFile);
-        audio.loop = true;
+    makeNewDoor();
 
-        doors.push(door);
-        audios.push(audio);
-
-        door.appendChild(audio);
-        doorDiv.appendChild(door);
-    }
-
+    scoreDiv.style.display = "block";
+    scoreDiv.innerHTML = "Score: 0";
     ticker = setInterval(tick, dt);
 }
 
 function endPlay() {
+    doors = [];
     document.getElementById("togglePlay").innerHTML = "Click to Play!";
     while (doorDiv.firstChild) {
         doorDiv.removeChild(doorDiv.firstChild);
     }
     clearInterval(ticker);
+
+    score = 0;
+    scoreDiv.style.display = "none";
 }
 
 function makeNewDoor() {
@@ -53,30 +49,46 @@ function makeNewDoor() {
     door.onclick = function() {
         close(door);
     }
-    return door;
+    var audio = new Audio(gertrudeFile);
+    audio.loop = true;
+
+    doors.push(door);
+
+    door.appendChild(audio);
+    doorDiv.appendChild(door);
 }
 
 function close(door) {
     door.src = closedDoorFile;
     door.style.cursor = "default";
+    // door.onclick = function() {
+    //     return;
+    // }
 
     var audio = door.children[0];
     audio.pause();
     audio.currentTime = 0;
+
+    score += 1;
+    scoreDiv.innerHTML = "Score: " + score;
+
+    if (doors.length < 21) {
+        makeNewDoor();
+    }
 }
 
 function open(door) {
     door.src = openDoorFile;
     door.style.cursor = "pointer";
+    door.onclick = function() {
+        close(door);
+    }
 }
 
 function tick() {
     n += 1;
-    // if (n < 20) {
-    //     return;
-    // }
     if (Math.random() < freq) {
-        var i = Math.floor(Math.random() * N_DOORS);
+        var i = Math.floor(Math.random() * doors.length);
         var door = doors[i];
         var audio = doors[i].children[0];
 
